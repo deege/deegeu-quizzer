76857eaf0ba58097d63da70de691219902de8919
DJ Spiess <dspiess@nso.edu>
2016-07-14 13:10:42 -0600

    Added code for trivia endpoints

---
+++ src/main/java/com/deegeu/model/TriviaQuestionArrayAccess.java
@@ -0,0 +1,207 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 DJ Spiess.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.deegeu.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Accesses the trivia question "database". This is used to test out the rest of
+ * the functionality. The database is an {@link ArrayList}.
+ * 
+ * @author DJ Spiess
+ */
+public class TriviaQuestionArrayAccess implements TriviaQuestionAccessible {
+    private ArrayList<TriviaQuestion> questionList;
+    
+    /**
+     * Constructor. Loads the array with trivia questions.
+     */
+    public TriviaQuestionArrayAccess() {
+        this.loadQuestionArray();
+    }
+    
+    @Override
+    public TriviaQuestion getQuestion(long index) {
+        // Force conversion from long to an int intentionally.
+        return ((int) index < questionList.size()) ? questionList.get((int) index) : null;
+    }
+    
+    @Override
+    public TriviaQuestion getRandomQuestion() {
+        int index = (new Random()).nextInt(questionList.size());
+        return questionList.get(index);
+    }
+    
+    @Override
+    public List<TriviaQuestion> getQuestionList(long offset) {
+        long start = offset; 
+        if (start < 0) {
+            start = 0;
+        }
+        if (start >= questionList.size()) {
+            start = questionList.size();
+        }
+        long end = start + 10;
+        if (end >= questionList.size()) {
+            end = questionList.size();
+        }
+        
+        return questionList.subList((int) start, (int) end);
+    }
+
+    @Override
+    public long getQuestionListSize() {
+        return questionList.size();
+    }
+
+    /*
+        Creates trivia questions and adds them to an array.
+    */
+    private void loadQuestionArray() {
+        questionList = new ArrayList<>();
+        questionList.add((new TriviaQuestionBuilder())
+            .id(0)
+            .question("How many feet are in a mile?")
+            .answerA("5260")
+            .answerB("5270")
+            .answerC("5280")
+            .answerD("5290")
+            .correctAnswer("C")
+            .hint("The altitude of Denver, Colorado")
+            .lastUpdated(new Date())    
+            .build()
+        );
+        questionList.add((new TriviaQuestionBuilder())
+            .id(1)
+            .question("What was the first toy advertised on television?")
+            .answerA("The Rubix Cube")
+            .answerB("Mr. Potato Head")
+            .answerC("Barbie")
+            .answerD("A hula hoop")
+            .correctAnswer("B")
+            .hint("Use your head on this one")
+            .lastUpdated(new Date())
+            .build()
+        );
+        questionList.add((new TriviaQuestionBuilder())
+            .id(2)
+            .question("The martial art of kung fu originated in which country?")
+            .answerA("Viet Nam")
+            .answerB("United States")
+            .answerC("Japan")
+            .answerD("China")
+            .correctAnswer("D")
+            .hint("Name most likely derives from the name of the Qin dynasty")
+            .lastUpdated(new Date())    
+            .build()
+        );
+        questionList.add((new TriviaQuestionBuilder())
+            .id(3)
+            .question("Which 1979 film included a spaceship called Nostromo?")
+            .answerA("The Emperor Strikes Back")
+            .answerB("Alien")
+            .answerC("The Black Hole")
+            .answerD("Star Trek: The Motion Picture")
+            .correctAnswer("B")
+            .hint("Not from this world")
+            .lastUpdated(new Date())    
+            .build()
+        );
+        questionList.add((new TriviaQuestionBuilder())
+            .id(4)
+            .question("Which country lies on the border between Spain and France?")
+            .answerA("Andorra")
+            .answerB("Luxemborg")
+            .answerC("England")
+            .answerD("Portugal")
+            .correctAnswer("A")
+            .hint("Go with your first guess")
+            .lastUpdated(new Date())    
+            .build()
+        );
+        questionList.add((new TriviaQuestionBuilder())
+            .id(5)
+            .question("CERN launched the very first website in what year?")
+            .answerA("1985")
+            .answerB("1960")
+            .answerC("1990")
+            .answerD("1995")
+            .correctAnswer("C")
+            .hint("Not before Star Wars")
+            .lastUpdated(new Date())    
+            .build()
+        );
+        questionList.add((new TriviaQuestionBuilder())
+            .id(6)
+            .question("What is the largest animal currently on Earth?")
+            .answerA("Elephant")
+            .answerB("Polar Bear")
+            .answerC("Blue Whale")
+            .answerD("Box Jellifish")
+            .correctAnswer("C")
+            .hint("Stick to the seas")
+            .lastUpdated(new Date())    
+            .build()
+        );
+        questionList.add((new TriviaQuestionBuilder())
+            .id(7)
+            .question("What was first feature length animated film?")
+            .answerA("Akira")
+            .answerB("Snow White and the Seven Dwarfs")
+            .answerC("Cinderella")
+            .answerD("Bambi")
+            .correctAnswer("B")
+            .hint("Bad apples")
+            .lastUpdated(new Date())    
+            .build()
+        );
+        questionList.add((new TriviaQuestionBuilder())
+            .id(8)
+            .question("The assasination that is said to have lead to World War I, occured in what city?")
+            .answerA("Paris")
+            .answerB("Sarajevo")
+            .answerC("Belgrade")
+            .answerD("Rome")
+            .correctAnswer("B")
+            .hint("Go east")
+            .lastUpdated(new Date())    
+            .build()
+        );
+        questionList.add((new TriviaQuestionBuilder())
+            .id(9)
+            .question("World War I flying ace Manfred von Richthofen is known by what nickname?")
+            .answerA("Snoopy")
+            .answerB("Bob")
+            .answerC("The Manchurian Candidate")
+            .answerD("The Red Baron")
+            .correctAnswer("D")
+            .hint("Royalty")
+            .lastUpdated(new Date())    
+            .build()
+        );
+    }
+}
---
+++ src/main/java/endpoints/TriviaQuestionEndpoint.java
@@ -0,0 +1,136 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 DJ Spiess.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package endpoints;
+
+import com.deegeu.model.TriviaQuestion;
+import com.deegeu.model.TriviaQuestionAccessible;
+import com.deegeu.model.TriviaQuestionArrayAccess;
+import java.util.Date;
+import java.util.List;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST endpoint for trivia questions. This supports random trivia questions, a list of 
+ * 10 questions, a random question, and a question count.
+ * 
+ * @author DJ Spiess
+ */
+@Path("/questions")
+final public class TriviaQuestionEndpoint {
+ 
+    final private Date questionsUpdatedDate = new Date();
+    final private TriviaQuestionAccessible dataAccess = new TriviaQuestionArrayAccess();
+    
+    /**
+     * Returns a list of at most 10 questions. The optional query parameter offset
+     * tells the method where in the list to start. If there are less than 10 
+     * questions in the list from the offset point, only the remaining questions 
+     * will be returned. If the offset is negative, the offset will start at 0. If 
+     * the offset is greater than the list size, an empty list will be returned. The
+     * header contains the list size, the number of questions returned, and the 
+     * total number of questions.
+     * 
+     * @param offset the starting point to get questions in list
+     * @return at most 10 questions starting at the offset as JSON in a HTTP response 
+     */
+    @GET
+    @Produces("text/json") 
+    public Response getQuestions(@QueryParam("offset") @DefaultValue("0") long offset) {
+        // Calculate the effective offset
+        long datasetSize = dataAccess.getQuestionListSize();
+        long start = offset; 
+        if (start < 0) {
+            start = 0;
+        }
+        if (start >= datasetSize) {
+            start = datasetSize;
+        }
+        
+        // Get the list of questions from starting point
+        List<TriviaQuestion> list = dataAccess.getQuestionList(start);
+        return Response.ok(list)
+                       .header("QuestionCount", datasetSize)
+                       .header("CurrentQuestionListSize", list.size())
+                       .header("Offset", start)
+                       .lastModified(questionsUpdatedDate)
+                       .build();
+    }
+    
+    /**
+     * Returns the total number of trivia questions in the database.
+     * 
+     * @return the question count as JSON in a HTTP response
+     */
+    @GET
+    @Path("count")
+    @Produces("text/json") 
+    public Response getQuestionCount() {
+        return Response.ok(dataAccess.getQuestionListSize())
+                       .header("QuestionCount", dataAccess.getQuestionListSize())
+                       .build();
+    }
+    
+    /**
+     * Returns a single question by id. If the query parameter is "random", a
+     * random question is returned. If the id is not found, 404 is returned. If
+     * the id cannot be parsed, 400 is returned.
+     * 
+     * @param idString either and index or the word "random"
+     * @return the question by id as JSON in a HTTP response
+     */
+    @GET
+    @Path("{id}")
+    @Produces("text/json")
+    public Response getQuestion(@PathParam("id") String idString) {
+        Response response;
+        if (idString.trim().equalsIgnoreCase("random")) {
+            TriviaQuestion question = dataAccess.getRandomQuestion();
+            response = Response.ok(question)
+                               .lastModified(question.getLastUpdated())
+                               .build();
+        } else {
+            try {
+                long index = Long.parseLong(idString);
+                if (index >= dataAccess.getQuestionListSize()) {
+                    response = Response.status(Response.Status.NOT_FOUND).build();    
+                } else {
+                    TriviaQuestion question = dataAccess.getQuestion(index);
+                    response = Response.ok(question)
+                            .lastModified(question.getLastUpdated())
+                            .build();
+                }
+            } catch (NumberFormatException ne) {
+                response = Response.status(Response.Status.BAD_REQUEST).build();
+            }
+            
+        }
+        return response;
+    }
+}
